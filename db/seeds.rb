8565bc019050df1108ccdfade5cb8466dc9591e4
RZeeshan <zrafique4@gmail.com>
2018-10-12 21:21:19 +0500

    fix the issue with seed not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.find_or_create_by!(email: "test@react.com", password: "")
+User.create!(email: "test@react.com", password: "test123")
