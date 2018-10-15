8e6cf86eed0546277d73faf3804be1687b268715
absar-intagleo <absar.mushtaq@ispokepakistan.com>
2018-10-15 12:03:56 +0500

    Today,Yeasterday date formating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+	def display_date(key)
+		if key.to_date == Date.today
+			"Today"
+		elsif key.to_date == Date.today - 1
+			"Yesterday"
+		else
+			key.strftime('%a %d %b %Y') 
+		end
+	end
 end
