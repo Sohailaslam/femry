4c1d53486a7e74fffa965846ca0bf7e3c6d1e928
RZeeshan <zrafique4@gmail.com>
2018-10-17 01:31:38 +0500

    validate admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,15 @@
 class Admin::DashboardController < ApplicationController
+	before_action :authenticate_user!
+	before_action :validate_admin
   def index
   	@users = User.all.order('created_at DESC')
+  end
+
+
+  private 
+  def validate_admin
+  	if current_user.is_admin == false
+  		redirect_to root_path, alert: "You are not permitted to access this Url"
+  	end
   end
 end
