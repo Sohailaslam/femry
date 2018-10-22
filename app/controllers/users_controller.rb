7a8257389c80106107c308a3ce45f24c59f22d5a
RZeeshan <zrafique4@gmail.com>
2018-10-22 23:54:09 +0500

    Add task cocoon gem changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
   def new
     current_user.incomplete_tasks if current_user.present?
-    @user = current_user.present? ? current_user : User.find(3) 
+    @user = current_user.present? ? current_user : User.find(3)
+    @user.tasks.create(task_date: Date.today, status: false) unless @user.tasks.present?
   end
 
   def update	
