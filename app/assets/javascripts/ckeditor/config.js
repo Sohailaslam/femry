0172e3ba5d26dc708e04ff412741b8e6112a8cb8
RZeeshan <zrafique4@gmail.com>
2018-11-01 00:20:05 +0500

    Ckeditor auto grow plugin

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,6 +3,10 @@ CKEDITOR.editorConfig = function (config) {
   // ... other configuration ...
   config.removePlugins = 'elementspath',
   config.resize_enabled = true,
+  config.extraPlugins = 'autogrow'
+  config.autoGrow_minHeight = 100,
+  config.autoGrow_maxHeight = 600,
+  config.autoGrow_bottomSpace = 20,
   config.toolbar_mini = [
     ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
   ];
