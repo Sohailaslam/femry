4008b548ad3fed408e5366570588d2e3a8ce283c
RZeeshan <zrafique4@gmail.com>
2018-11-27 00:19:07 +0500

    Plans merged

--- db/migrate/20181126071040_add_plan_to_user.rb
+++ db/migrate/20181126071040_add_plan_to_user.rb
@@ -1,6 +1,6 @@
 class AddPlanToUser < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :plan, foreign_key: true
+    add_reference :users, :plan, foreign_key: true, default: 1
     add_column :users, :last_renenwed, :datetime
     add_column :users, :status, :integer
   end
