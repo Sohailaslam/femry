89305100fc39aa8590182245739e655038d7a63c
RZeeshan <zrafique4@gmail.com>
2018-12-03 21:32:28 +0500

    Fix the issue with empty tasks showing in public feed page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
 
   def public_feed
-    @grouped_tasks = Task.includes(:user).where('users.public_task = ? AND is_deleted = ? AND status = ?', true, false, true).references(:user).order("task_date DESC").order(:sort).where.not(title: nil).group_by(&:task_date)
+    @grouped_tasks = Task.includes(:user).where('users.public_task = ? AND is_deleted = ? AND status = ?', true, false, true).references(:user).order("task_date DESC").order(:sort).where.not(title: nil).where.not(title: '').group_by(&:task_date)
     unless @grouped_tasks.present?
       @grouped_tasks = {Time.now.in_time_zone(Time.zone.name).to_date => []}.merge!(@grouped_tasks)
     end
